bb3fce281bd202e675d4b3de8a87c95a79a53148
AndreyM <andrey@unplugged-systems.com>
2022-12-06 09:37:49 +0200

    first commit

--- src/main/java/com/up/up_shop_test/model/GoodsType.java
+++ src/main/java/com/up/up_shop_test/model/GoodsType.java
@@ -1,11 +1,18 @@
 package com.up.up_shop_test.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Document("goods_type")
-public record GoodsType(
-        @Id String id,
-        String type) {
+public class GoodsType {
+        @Id
+        private String id;
+        private String type;
 
 }
